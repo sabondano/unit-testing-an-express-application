9f8c1ba08f1f2ae56a44b4e809afca543f41a6bb
Sebastian Abondano <sabondano1@gmail.com>
2015-11-14 15:00:41 -0700

    Add and setup body-parser package

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const path = require('path');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('static'));
 
 app.set('port', process.env.PORT || 3000);
